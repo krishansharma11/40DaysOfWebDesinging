b03b2385876cda34f0c8c7cc10482c0dd0b2b650
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-11-23 13:32:18 +0530

    Initial Comment

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 4) {
-        counter = 1;
-    }
-}, 5000);
+function myHello() {
+    document.getElementById('demo')
+        .innerHTML = 'Called External JavaScript'
+}
